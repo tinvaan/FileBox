8b8f5ae0917cdb058f300db3c6b0bf95e43cd4e4
Harish Navnit <harishnavnit@gmail.com>
2024-07-30 22:25:48 +0800

    Enable mongomock gridfs integration

--- run.py
+++ run.py
@@ -3,16 +3,21 @@
 import mongoengine as db
 import mongomock as MockDB
 
+from mongomock.gridfs import enable_gridfs_integration
+
 from filebox.views import app
 from filebox.urls import Routes
 
 
 Routes.setup()
+
+enable_gridfs_integration()
 db.connect(
     app.config.get('MONGODB_DB'),
     host=app.config.get('MONGODB_HOST'),
     mongo_client_class=MockDB.MongoClient
 )
+
 app.run(
     debug=True,
     port=app.config.get('FILEBOX_PORT'),
