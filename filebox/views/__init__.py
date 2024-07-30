72c3326ee8de266c26b1342d1eeb971a719e2a68
Harish Navnit <harishnavnit@gmail.com>
2024-07-30 12:13:34 +0800

    Setup url routes

--- filebox/views/__init__.py
+++ filebox/views/__init__.py
@@ -1,10 +1,7 @@
 
 from flask import Flask
 
-from .uploads import uploads
 
 app = Flask('filebox')
 app.config.from_object('filebox.config')
 app.config.from_pyfile('config.py', silent=True)
-
-app.register_blueprint(uploads)
--- run.py
+++ run.py
@@ -4,14 +4,15 @@ import mongoengine as db
 import mongomock as MockDB
 
 from filebox.views import app
+from filebox.urls import Routes
 
 
+Routes.setup()
 db.connect(
     app.config.get('MONGODB_DB'),
     host=app.config.get('MONGODB_HOST'),
     mongo_client_class=MockDB.MongoClient
 )
-
 app.run(
     debug=True,
     port=app.config.get('FILEBOX_PORT'),
