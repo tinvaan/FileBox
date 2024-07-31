11f3185bfd771cba38681264a9cd5229dd7fdbd7
Harish Navnit <harishnavnit@gmail.com>
2024-07-31 17:19:45 +0800

    Support query params for fetch uploads endpoint

---
+++ tests/test_views_uploads.py
@@ -0,0 +1,106 @@
+
+import ipdb
+import json
+import mimetypes
+import unittest
+import mongoengine as db
+import mongomock as MockDB
+
+from os import path
+from mongomock.gridfs import enable_gridfs_integration
+
+from . import Env, fixtures
+from filebox import app
+from filebox.urls import Routes
+from filebox.models.uploads import FileBlob, FileUpload
+
+
+class Fixtures:
+    @staticmethod
+    def blobs():
+        for fx in [fixtures.get('test.png'), fixtures.get('test.pdf'), fixtures.get('test.jpg')]:
+            mime = mimetypes.guess_type(fx)[0]
+            blob = FileBlob(name=path.basename(fx), size=path.getsize(fx), type=mime)
+            with open(fx, 'rb') as fd:
+                blob.uri.put(fd, content_type=mime)
+            blob.save()
+
+    @staticmethod
+    def uploads():
+        for blob in FileBlob.objects.all():
+            FileUpload(blob=blob.id).save()
+
+    @staticmethod
+    def add(hidden=False):
+        # Pick a fixture
+        f = fixtures.get('test.pdf')
+
+        # Create a blob entry
+        mime = mimetypes.guess_type(f)[0]
+        blob = FileBlob(name='fixture', size=path.getsize(f), type=mime)
+        with open(f, 'rb') as fd:
+            blob.uri.put(fd, content_type=mime)
+        blob.save()
+
+        # Create an upload
+        FileUpload(hidden=hidden, blob=blob.id).save()
+
+    @staticmethod
+    def seed():
+        Fixtures.blobs()
+        Fixtures.uploads()
+
+
+class TestUploads(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        Routes.setup()
+        enable_gridfs_integration()
+
+        app.config['TESTING'] = True
+        cls.env = Env.load(app.config)
+        cls.app = app.test_client()
+
+    def setUp(self):
+        self.url = self.env.host
+        self.conn = db.connect(
+            'testdb',
+            host=app.config.get('MONGODB_HOST'),
+            mongo_client_class=MockDB.MongoClient
+        )
+        Fixtures.seed()
+
+    def test_fetch_uploads(self):
+        r = self.app.get(self.url + '/uploads')
+
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(json.loads(r.json)), 3)
+
+    def test_hidden_uploads(self):
+        Fixtures.add(hidden=True)
+
+        r = self.app.get(self.url + '/uploads')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(json.loads(r.json)), 3)
+
+        r = self.app.get(self.url + '/uploads?all=false')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(json.loads(r.json)), 3)
+
+        r = self.app.get(self.url + '/uploads?all=true')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(json.loads(r.json)), 4)
+
+        r = self.app.get(self.url + '/uploads?hidden=false')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(json.loads(r.json)), 3)
+
+        r = self.app.get(self.url + '/uploads?hidden=true')
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(len(json.loads(r.json)), 4)
+
+    def tearDown(self) -> None:
+        self.db = db.get_db()
+        self.db.drop_collection('blobs')
+        self.db.drop_collection('uploads')
+        self.conn.drop_database(self.db.name)
