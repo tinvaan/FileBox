fc05fb81de61b70f03eaa5eb7f9c49cc68186766
Harish Navnit <harishnavnit@gmail.com>
2024-08-01 17:43:35 +0800

    Show blob details in list response

--- filebox/views/uploads.py
+++ filebox/views/uploads.py
@@ -22,7 +22,7 @@ class Uploads(MethodView):
         show_all = False if str(request.args.get('all', 'false')).lower() == 'false' else True
         show_hidden = False if str(request.args.get('hidden', 'false')).lower() == 'false' else True
         kwargs = {} if show_all or show_hidden else {'hidden': show_hidden}
-        return jsonify(FileUpload.objects.filter(**kwargs).to_json())
+        return jsonify([doc.to_json() for doc in FileUpload.objects.filter(**kwargs)])
 
     def post(self):
         try:
