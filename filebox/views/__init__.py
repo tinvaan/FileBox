75c4f2303f5ef3f4d18f4861377e64f6a43e5ab2
Harish Navnit <harishnavnit@gmail.com>
2024-08-01 16:38:10 +0800

    index redirects to /uploads

--- filebox/views/__init__.py
+++ filebox/views/__init__.py
@@ -1,7 +1,12 @@
 
 from flask import Flask
+from flask import redirect, url_for
 
 
 app = Flask('filebox')
 app.config.from_object('filebox.config')
 app.config.from_pyfile('config.py', silent=True)
+
+@app.route('/')
+def home():
+    return redirect(url_for('uploads.items'))
