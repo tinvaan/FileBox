3eb39727b6b2939125aedd2f2d0a3b256df6e431
Harish Navnit <harishnavnit@gmail.com>
2024-08-01 11:10:01 +0800

    try to return json responses instead of json string responses wherever possible

--- filebox/utils.py
+++ filebox/utils.py
@@ -1,7 +1,14 @@
 
+import json
+
 from flask import make_response
 from flask import jsonify as JSON
 
 
 def jsonify(content, status=200):
+    try:
+        content = json.loads(content)
+    except Exception:
+        pass
+
     return make_response(JSON(content), status)
